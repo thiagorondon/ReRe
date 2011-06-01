fe7dc80d040290fd054b534f428e2907d6358bce
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2011-06-01 12:41:10 -0300

    POD.
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

--- lib/ReRe.pm
+++ lib/ReRe.pm
@@ -21,11 +21,30 @@ has server => (
     predicate => 'has_server',
 );
 
+=head1 DESCRIPTION
+
+ReRe is a simple redis rest interface write in Perl and L<Mojolicious</a>,
+with some features like:
+
+=over
+
+=item Config file for store users and access control list.
+
+=item REST interface to make your life more easy in some world.
+
+=item Support to run as daemon (simple web-service), CGI, FastCGI or PSGI.
+
+=item Simple to install and use
+
+=back
+
+More information, you can read in L<http://www.rere.com.br>.
+
 =head1 METHODS
 
 =head2 start
 
-Start masterplan.
+Start ReRe.
 
 =cut
 
