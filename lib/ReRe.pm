4919cc8ad48c17c76f3412802178f258b4fb2ccf
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2011-05-31 21:19:43 -0300

    Adicionando métodos para iniciar o ReRe.
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

--- lib/ReRe.pm
+++ lib/ReRe.pm
@@ -2,9 +2,23 @@
 package ReRe;
 
 use Moose;
+use ReRe::ACL;
 
 # ABSTRACT: Simple Redis Rest Interface
 # VERSION
+
+has acl => (
+    is => 'ro',
+    isa => 'ReRe::ACL',
+    lazy => 1,
+    default => sub { ReRe::ACL->new( { file => 'etc/users.conf' }) }
+);
+
+sub start {
+    my $self = shift;
+    $self->acl->process;
+}
+
 
 1;
 
