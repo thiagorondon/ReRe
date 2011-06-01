73593cda248b8b4dbcb23495709ef1d1cbf8f304
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2011-05-31 21:20:10 -0300

    Ajustando o rere_server.pl para efetuar autenticação.
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

--- rere_server.pl
+++ rere_server.pl
@@ -3,22 +3,30 @@
 # Thiago Rondon <thiago@aware.com.br>
 #
 
+BEGIN { push( @INC, './lib' ) }
+
 use Mojolicious::Lite;
+use ReRe;
+
+my $rere = ReRe->new;
+$rere->start;
 
 get '/login' => sub {
-    my $self = shift;
+    my $self     = shift;
     my $username = $self->param('username') || '';
     my $password = $self->param('password') || '';
 
-    return $self->render unless $username eq 'teste' and $password eq 'teste';
-    $self->session(name => $username);
-    $self->render_json({ login => 1 });
+    return $self->render_json( { login => 0 } )
+      unless $rere->acl->auth( $username, $password );
+    
+    $self->session( name => $username );
+    $self->render_json( { login => 1 } );
 } => 'login';
 
 get '/logout' => sub {
     my $self = shift;
-    $self->session(expires => 1);
-    $self->render_json({ logout => 1 });
+    $self->session( expires => 1 );
+    $self->render_json( { logout => 1 } );
 } => 'logout';
 
 app->start;
